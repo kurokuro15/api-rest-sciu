f52cab0911fd090f9cc255a97619f04dc6e2fdad
kurokuro15 <kurokuro15000@gmail.com>
2022-05-02 14:43:10 -0400

    try index

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-print_r('hello word');
+require_once 'Models/Model.php';
+
+$conection = new Model;
+$id = 26576198;
+$res = $conection->query("SELECT id_cedula as 'cedula', nombre1 as 'nombre', factura, SUM(pagos.monto) as monto FROM alumnos 
+JOIN emisiones ON id_cedula = id_cedul JOIN pagos ON idregistro = idregistr 
+WHERE id_cedula = $id 
+GROUP BY id_cedula, nombre1, factura;");
+
+print_r($res);
