38a4279ff1e759d60e309de1ab255ba119c58176
kurokuro15 <kurokuro15000@gmail.com>
2022-05-05 14:40:50 -0400

    add Route class to base system

--- base/routers/Route.php
+++ base/routers/Route.php
@@ -1,5 +1,48 @@
 <?
+
 namespace base\routers;
+
 /**
  * Este archivo llevará la clase Route quien tendrá la definición de una ruta.
  */
+/**
+ * Modelo de una ruta
+ */
+
+/**
+ * @param $method string [GET,POST,PUT,DELETE]
+ */
+class Route
+{
+	private $method;
+	private $pattern;
+	private $callback;
+	private $listMethod = ['GET', 'POST', 'PUT', 'DELETE'];
+
+	function __construct($method, $pattern, $callback)
+	{
+		$this->method = $this->validateMethod($method);
+		$this->pattern = $pattern;
+		$this->callback = $callback;
+	}
+	public function getMethod()
+	{
+		return $this->method;
+	}
+	public function getPattern()
+	{
+		return $this->pattern;
+	}
+	public function getCallback()
+	{
+		return $this->callback;
+	}
+
+	private function validateMethod($method) {
+		if (in_array(strtoupper($method), $this->listMethod)) 
+				return $method;
+
+		throw new \Exception('Invalid Method Name');
+}
+
+}
