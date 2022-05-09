dd92d88e32e535a7008c653e91c76077c2ead717
kurokuro15 <kurokuro15000@gmail.com>
2022-05-09 16:36:29 -0400

    adding some logic to routers to try

--- routers/router.php
+++ routers/router.php
@@ -1,41 +1,50 @@
 <?php
+
 /**
  * Definiremos las rutas acá.
  */
 
- $router->get("/estudiantes", function($params){
-		print_r("Hola desde la ruta estudiantes");
+use \api\controllers\FacturaController;
+
+$router->get("/estudiantes", function ($params) {
+
+});
+
+$router->get("/estudiantes/:cedula", function ($params) {
+	$factura = new FacturaController;
+	try {
+
+		$body = $factura->get($params);
 		$response = $GLOBALS['response'];
-		if($response){ 
+		if ($body) {
 			$response->status(200);
-			$response->send(["Hola" => "Adiós"]);
+			$response->send($body);
 		}
- });
+	} catch (ValueError $err) {
+		$response->status(404);
+		$response->send("{\"error\":\"$err\"}");
+	}
+});
 
- $router->get("/estudiantes/:cedula", function($params){
-	if(!empty($params)) 
-		print_r($params['cedula']);
- });
-
- $router->get("/ordenes", function($params){
-	 print_r('Hola desde la ruta Ordenes');
-	if(!empty($params))
+$router->get("/ordenes", function ($params) {
+	print_r('Hola desde la ruta Ordenes');
+	if (!empty($params))
 		print_r('Hola desde la ruta Ordenes con el estudiante: <br>');
-		print_r('Hola desde la ruta Ordenes con el recibo: <br>');	
- });
- $router->get("/ordenes/:id_registro", function($params){
-	if(!empty($params)) 
+	print_r('Hola desde la ruta Ordenes con el recibo: <br>');
+});
+$router->get("/ordenes/:id_registro", function ($params) {
+	if (!empty($params))
 		print_r($params['id_registro']);
- });
+});
 
- $router->get("/recibos", function($params){
+$router->get("/recibos", function ($params) {
 	print_r('Hola desde la ruta Recibos');
-	if(!empty($params)) 
+	if (!empty($params))
 		print_r('Hola desde la ruta recibos con el estudiante: <br>');
-		print_r('Hola desde la ruta recibos con la orden: <br>');	
- });
+	print_r('Hola desde la ruta recibos con la orden: <br>');
+});
 
- $router->get("/recibos/:id", function($params){
-	if(!empty($params)) 
+$router->get("/recibos/:id", function ($params) {
+	if (!empty($params))
 		print_r($params['id']);
- });
+});
