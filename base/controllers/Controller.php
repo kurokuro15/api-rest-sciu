c114197667327c7cd27a97a745803d121a03b2bd
kurokuro15 <kurokuro15000@gmail.com>
2022-05-09 16:37:39 -0400

    adding base construct to controller base

--- base/controllers/Controller.php
+++ base/controllers/Controller.php
@@ -1,11 +1,31 @@
 <?php
+
 namespace base\controllers;
+
+use base\https\Response;
+use base\https\Request;
+
 /**
  * Archivo de Controlador base.
  */
-class Controller {
-	function __construct()
+class Controller
+{
+	/**
+	 * Request Class.
+	 */
+	public $request;
+
+	/**
+	 * Response Class.
+	 */
+	public $response;
+
+	/**
+	 *  Construct
+	 */
+	public function __construct()
 	{
-		
+		$this->request = new Request;
+		$this->response = new Response;
 	}
 }
