6fc9de31deb403cc22516dc1bb56be8e63effd71
kurokuro15 <kurokuro15000@gmail.com>
2022-05-10 12:28:56 -0400

    add getAll method to Student Model and some validations

--- models/Student.php
+++ models/Student.php
@@ -1,32 +1,49 @@
 <?php
+
 namespace api\Models;
-include(dirname(dirname(__FILE__)) . '/base/models/Model.php');
+//include para probar no más xD 
 use base\models\Model;
+use Error;
 use ValueError;
 
-class Student extends Model {
-/**
- * Retrieve a Student by 'cedula'
- */
-	function get($id_cedula){
+class Student extends Model
+{
+	/**
+	 * Retrieve a Student by 'cedula'
+	 */
+	function get($cedula)
+	{
 		//Validate param
-		if(!isset($id_cedula) || (int) $id_cedula === 0) {
-			throw new ValueError("Cedula not is a number");
+		if (!isset($cedula) || (int) $cedula === 0) {
+			throw new ValueError("Cedula not is a valid number", 401);
 		}
 		// map param in a array
-		$param = [":id_cedula" => $id_cedula];
+		$param = [":cedula" => $cedula];
 		// prepare query
-		$query = "SELECT id_cedula as cedula, id_carrera as career_id, nombrecarrera as career, nombre1 as first_name, nombre2 as middle_name, apellido1 as last_name, apellido2 as sur_name, semestre as semester, fechainscr as reg_date FROM alumnos JOIN carreras ON id_carrera = id_carrer  WHERE alumnos.id_cedula = :id_cedula;";
+		$query = "SELECT id_cedula as cedula, id_carrera as career_id, nombrecarrera as career, nombre1 as first_name, nombre2 as middle_name, apellido1 as last_name, apellido2 as sur_name, semestre as semester, fechainscr as reg_date FROM alumnos JOIN carreras ON id_carrera = id_carrer  WHERE alumnos.id_cedula = :cedula;";
 		// retrieve data and save in an variable
-		$data = parent::query($query,$param);
-		if(is_array($data)){
+		$data = parent::query($query, $param);
+		if (is_array($data)) {
 			// we mapping properties of class to use this info. And send Json object form return.
-			foreach($data[0] as $prop => $value) {
+			foreach ($data[0] as $prop => $value) {
 				$this->$prop = $value;
 			}
+		} else {
+			throw new Error("data not found", 404);
 		}
+		// if all it's okay return the student.
+		return $data[0];
+	}
+
+	function getAll($page = 0, $items = 5){
+		$pagination = parent::pagination($page,$items);
+
+		$query = "SELECT cedula as cedula, nombre1 as first_name, apellido1 as last_name, semestre as semester, fechainscr as reg_date, nombrecarrera as career FROM alumnos JOIN carreras ON id_carrera = id_carrer  ORDER BY reg_date DESC LIMIT :limit OFFSET :offset";
+		$data = parent::query($query,$pagination);
+		if (count($data)<1) {
+			throw new Error("data not found", 404);
+		}
+		// if all it's okay return the student.
+		return $data;
 	}
 }
-$student = new Student;
-$student->get(26576198);
-var_dump($student->first_name);
