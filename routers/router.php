0c4d119a8b9a981131873ab379162edcd5f6126d
kurokuro15 <kurokuro15000@gmail.com>
2022-05-10 14:51:07 -0400

    delete test routes

--- routers/router.php
+++ routers/router.php
@@ -2,13 +2,11 @@
 /**
  * Definiremos las rutas acá.
  */
-use \api\Models\Student;
 use \api\Controllers\StudentController;
+
 $router->get("/estudiantes", function ($params) {
-	$student = new Student;
-	$data = $student->getAll();
-	$response = $GLOBALS['response'];
-	$response->send($data);
+	$student = new StudentController;
+	$student->get($params);
 });
 
 $router->get("/estudiantes/:cedula", function($params) {
@@ -16,25 +14,3 @@ $router->get("/estudiantes/:cedula", function($params) {
 	$student->retrieve($params);
 });
 
-$router->get("/ordenes", function ($params) {
-	print_r('Hola desde la ruta Ordenes');
-	if (!empty($params))
-		print_r('Hola desde la ruta Ordenes con el estudiante: <br>');
-	print_r('Hola desde la ruta Ordenes con el recibo: <br>');
-});
-$router->get("/ordenes/:id_registro", function ($params) {
-	if (!empty($params))
-		print_r($params['id_registro']);
-});
-
-$router->get("/recibos", function ($params) {
-	print_r('Hola desde la ruta Recibos');
-	if (!empty($params))
-		print_r('Hola desde la ruta recibos con el estudiante: <br>');
-	print_r('Hola desde la ruta recibos con la orden: <br>');
-});
-
-$router->get("/recibos/:id", function ($params) {
-	if (!empty($params))
-		print_r($params['id']);
-});
