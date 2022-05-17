7d9d52ca717fa2dbcfdbe265403c103f98feb0ec
kurokuro15 <kurokuro15000@gmail.com>
2022-05-17 07:47:31 -0400

    fix: indentation router sheet

--- routers/router.php
+++ routers/router.php
@@ -4,19 +4,20 @@
  * Definiremos las rutas acá.
  */
 
+use api\Controllers\StudentController;
 use api\controllers\ChargeController;
-use \api\Controllers\StudentController;
-use \api\Controllers\OrderController;
+use api\Controllers\OrderController;
 use api\controllers\ReceiptController;
 
 /**
  *  Students Endpoints
  */
+//get all
 $router->get("/estudiantes", function ($params) {
 	$student = new StudentController;
 	$student->get($params);
 });
-
+//retrieve one
 $router->get("/estudiantes/:cedula", function ($params) {
 	$student = new StudentController;
 	$student->retrieve($params);
@@ -25,67 +26,63 @@ $router->get("/estudiantes/:cedula", function ($params) {
  *  Students Endpoints
  */
 
-
 /**
  *  Orders Endpoints
  */
+//get all
 $router->get("/ordenes", function ($params) {
 	$order = new OrderController;
 	$order->get($params);
 });
-
+//retrieve one
 $router->get("/ordenes/:order", function ($params) {
 	$order = new OrderController;
 	$order->retrieve($params);
 });
-
-// POST Method
-$router->post("/ordenes", function($params){
+// create one
+$router->post("/ordenes", function ($params) {
 	$order = new OrderController;
 	$order->create($params);
 });
-
 /**
  *  Orders Endpoints
  */
 
- 
 /**
  *  Receipts Endpoints
  */
-$router->get("/recibos/:receipt", function ($params) {
-	$receipt = new ReceiptController;
-	$receipt->retrieve($params);
-});
-
+//get all
 $router->get("/recibos", function ($params) {
 	$receipt = new ReceiptController;
 	$receipt->get($params);
 });
+//retrieve one
+$router->get("/recibos/:receipt", function ($params) {
+	$receipt = new ReceiptController;
+	$receipt->retrieve($params);
+});
 /**
  *  Receipts Endpoints
  */
 
-
- /**
+/**
  *  Charges Endpoints
  */
-
-$router->get("/cobros/:charge", function ($params) {
-	$charge = new ChargeController;
-	$charge->retrieve($params);
-});
-
+//get all
 $router->get("/cobros", function ($params) {
 	$charge = new ChargeController;
 	$charge->get($params);
 });
-
+//retrieve one
+$router->get("/cobros/:charge", function ($params) {
+	$charge = new ChargeController;
+	$charge->retrieve($params);
+});
+//create one
 $router->post("/cobros", function ($params) {
 	$charge = new ChargeController;
 	$charge->create($params);
 });
-
 /**
  *  Charges Endpoints
  */
