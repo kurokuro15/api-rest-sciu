6dd4e03c98ebe2d56bb1b5bedf463016b9aa8d16
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-05-24 15:46:47 -0400

    Add another JWT class

---
+++ base/helpers/Encoder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace base\helpers;
+
+class Encoder
+{
+  static function base64UrlEncode($str)
+  {
+    return rtrim(strtr(base64_encode($str), "+/", "-_"), "=");
+  }
+
+  static public function base64UrlDecode($str)
+  {
+    return
+      base64_decode(
+        str_pad(
+          strtr($str, "-_", "+/"),
+          strlen($str) % 4,
+          "=",
+          STR_PAD_RIGHT,
+        ),
+      );
+  }
+}
---
+++ base/helpers/JWT.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace base\helpers;
+
+use base\helpers\Config;
+use base\helpers\Encoder;
+use Error;
+
+class JWT
+{
+  private $header;
+  private $payload;
+  private $secret;
+  private $tokenParts;
+
+  public function __construct(
+    $header = null,
+    $payload = null,
+    $secret = null
+  ) {
+    $this->header = $header ?? ["alg" => "HS256", "typ" => "JWT"];
+    $this->payload = $this->genPayload($payload ?? []);
+    $this->secret = $secret ?? Config::getConfigFileData('pepper')['pepperoni'];
+    $this->encode();
+  }
+
+  private function genPayload($data)
+  {
+    $twoHours = 60 * 60; // 7.200 seconds
+    $exp  = time() + $twoHours;
+    $payload =  [];
+    // set exp time
+    $payload['exp'] = $exp;
+
+    // set data
+    foreach ($data as $k  => $v) {
+      $payload[$k] = $v;
+    }
+
+    return $payload;
+  }
+
+  private function encode()
+  {
+    $header = Encoder::base64UrlEncode(json_encode($this->header));
+    $payload = Encoder::base64UrlEncode(json_encode($this->payload));
+
+    $this->tokenParts = [
+      'header' => $header,
+      'payload' => $payload
+    ];
+  }
+
+  public function getSignature()
+  {
+    $signature = hash_hmac(
+      "sha256",
+      "{$this->tokenParts['header']}.{$this->tokenParts['payload']}",
+      $this->secret,
+      true
+    );
+
+    return Encoder::base64UrlEncode($signature);
+  }
+
+  public function getToken()
+  {
+    return "{$this->tokenParts['header']}.{$this->tokenParts['payload']}.{$this->getSignature()}";
+  }
+
+  public function validateJWT($token)
+  {
+    $tokenParts = explode(".", $token);
+    $header = $tokenParts[0];
+    $payload = json_decode(Encoder::base64UrlDecode(($tokenParts[1])), true);
+    $signature = $tokenParts[2];
+
+    $expired = $payload['exp'] - time() < 0;
+    if ($expired) {
+      throw new Error("The JWT sent is expired.", 403);
+    }
+
+    $jwt = new self($header, $payload);
+
+    if ($signature !== $jwt->getSignature()) {
+      throw new Error("The JWT sent is not valid. Invalid signature: $signature", 403);
+    }
+
+    return true;
+  }
+}
