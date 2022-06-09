aeeccca165123b2f78b0188d5cce44714b3afc35
kurokuro15 <kurokuro15000@gmail.com>
2022-06-09 09:47:45 -0400

    add delete

--- public/index.php
+++ public/index.php
@@ -26,11 +26,10 @@ $response->setHeader('Content-Type: application/json; charset=UTF-8');
  
 //trampita para validar el preflight
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-	// The request is using the POST method
 	header("HTTP/1.1 200 OK");
 	header('Access-Control-Allow-Origin:  http://localhost:3000');
 	header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-	header('Access-Control-Allow-Methods:  POST, PUT');
+	header('Access-Control-Allow-Methods:  POST, PUT, DELETE');
 	return;
 }
 // resquest. Acá tocaría validad que sea un método válido CREO...
